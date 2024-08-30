d48c5c7a79c1bafe361a5cf2a118c65e105db1f3
sajjadhossain <sajjadhossain0756@gmail.com>
2024-08-30 22:04:28 +0600

    task-1 task-2 and task-3 complete

---
+++ index.js
@@ -0,0 +1,25 @@
+// task-1 start here
+const fruits = ["mango","banana","orange","apple","watermelon"];
+
+console.log(fruits[3]);
+
+fruits[2] = "jambura";
+
+console.log(fruits);
+// task-2 start here
+const touristArea = ["bandarban","cox'sbazar","patuakhali"];
+
+touristArea.push("kagrachari");
+
+console.log(touristArea);
+
+touristArea.push("sundarban","shylet");
+
+console.log(touristArea);
+
+touristArea.pop();
+console.log(touristArea);
+// task-3 start here
+const booklist = ["bangla","english","Math","Physics","chemistry"];
+
+console.log(booklist.includes("javascript"));
