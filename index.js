6be5002b6f83132ea17ee86b2df55e51fc83215c
sajjadhossain <sajjadhossain0756@gmail.com>
2024-08-30 22:14:55 +0600

    task-4 complete

--- index.js
+++ index.js
@@ -19,7 +19,19 @@ console.log(touristArea);
 
 touristArea.pop();
 console.log(touristArea);
+
 // task-3 start here
 const booklist = ["bangla","english","Math","Physics","chemistry"];
 
 console.log(booklist.includes("javascript"));
+
+// task-4 start here
+const number1 = 400;
+const numbers = [1, 3, 5, 7, 10];
+const number2 = ["one","three","four","five"];
+const name = "sajjad";
+
+console.log(Array.isArray(number1));
+console.log(Array.isArray(numbers));
+console.log(Array.isArray(number2));
+console.log(Array.isArray(name));
